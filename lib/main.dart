6c963eacf5f07b6f4e0b603f096a8dec7485b375
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-08 11:57:02 +0500

    Added main file

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './providers/great_places.dart';
+
+import './screens/place_list_screen.dart';
+import './screens/add_place_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch()
+              .copyWith(primary: Colors.indigo, secondary: Colors.amber),
+        ),
+        home: const PlaceListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+        },
+      ),
+    );
+  }
+}

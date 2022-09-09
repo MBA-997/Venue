2f6c1c71451760da8cc1f0768ff1659b45b76d7d
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-09 12:38:55 +0500

    Added camera access and ability to take pictures

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,9 +1,15 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
-  const ImageInput({Key? key}) : super(key: key);
+  final Function onSelectImage;
+
+  const ImageInput({Key? key, required this.onSelectImage}) : super(key: key);
 
   @override
   State<ImageInput> createState() => _ImageInputState();
@@ -11,6 +17,23 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File? _storedImage;
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final imageFile =
+        await picker.pickImage(source: ImageSource.camera, maxWidth: 600);
+
+    if (imageFile == null) return;
+
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await imageFile.saveTo('$appDir/$fileName');
+    widget.onSelectImage(savedImage);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,22 +44,22 @@ class _ImageInputState extends State<ImageInput> {
           height: 100,
           decoration:
               BoxDecoration(border: Border.all(color: Colors.grey, width: 1)),
+          alignment: Alignment.center,
           child: _storedImage == null
               ? Image.file(
                   _storedImage!,
                   fit: BoxFit.cover,
                   width: double.infinity,
                 )
-              : Text('No Image Taken'),
-          alignment: Alignment.center,
+              : const Text('No Image Taken'),
         ),
-        SizedBox(
+        const SizedBox(
           width: 10,
         ),
         Expanded(
           child: TextButton.icon(
-              icon: Icon(Icons.camera),
-              onPressed: () {},
+              icon: const Icon(Icons.camera),
+              onPressed: _takePicture,
               label: Text(
                 'Enter image URL',
                 textAlign: TextAlign.center,

59c104f2919253b15e5b09ccadb3c454da203039
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-09 20:02:00 +0500

    Added location and map integration

--- lib/screens/place_list_screen.dart
+++ lib/screens/place_list_screen.dart
@@ -21,22 +21,32 @@ class PlaceListScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        child:
-            const Center(child: Text('Got no places yet, start adding some!')),
-        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
-            ? ch as Widget
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (ctx, i) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(greatPlaces.items[i].image),
-                  ),
-                  title: Text(greatPlaces.items[i].title),
-                  onTap: () {
-                    //detail page
-                  },
-                ),
+      body: FutureBuilder(
+        future:
+            Provider.of<GreatPlaces>(context, listen: false).fetchAndSetData(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<GreatPlaces>(
+                child: const Center(
+                    child: Text('Got no places yet, start adding some!')),
+                builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+                    ? ch as Widget
+                    : ListView.builder(
+                        itemCount: greatPlaces.items.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage:
+                                FileImage(greatPlaces.items[i].image),
+                          ),
+                          title: Text(greatPlaces.items[i].title),
+                          onTap: () {
+                            //detail page
+                          },
+                        ),
+                      ),
               ),
       ),
     );

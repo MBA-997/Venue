8bb796b95e8099ab7edf2af91fde398bf05e901b
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-09 12:40:55 +0500

    Added List view builder for places with camera taken image

--- lib/screens/place_list_screen.dart
+++ lib/screens/place_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:venue/providers/great_places.dart';
 
 import '../screens/add_place_screen.dart';
 
@@ -9,17 +11,34 @@ class PlaceListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Your Places'),
+        title: const Text('Your Places'),
         actions: [
           IconButton(
             onPressed: () {
               Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
             },
-            icon: Icon(Icons.add),
+            icon: const Icon(Icons.add),
           )
         ],
       ),
-      body: Center(child: CircularProgressIndicator()),
+      body: Consumer<GreatPlaces>(
+        child:
+            const Center(child: Text('Got no places yet, start adding some!')),
+        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+            ? ch as Widget
+            : ListView.builder(
+                itemCount: greatPlaces.items.length,
+                itemBuilder: (ctx, i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(greatPlaces.items[i].image),
+                  ),
+                  title: Text(greatPlaces.items[i].title),
+                  onTap: () {
+                    //detail page
+                  },
+                ),
+              ),
+      ),
     );
   }
 }

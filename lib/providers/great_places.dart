972455622da5de3d9773a08d87f992927896d4a7
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-10 12:20:52 +0500

    Integrated location_helper to add location support

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 
 import '../helpers/db_helper.dart';
+import '../helpers/location_helper.dart';
 import '../models/place.dart';
 
 class GreatPlaces with ChangeNotifier {
@@ -11,12 +12,20 @@ class GreatPlaces with ChangeNotifier {
     return [..._items];
   }
 
-  void addPlace(String pickedTitle, File pickedImage) {
+  Future<void> addPlace(String pickedTitle, File pickedImage,
+      PlaceLocation pickedLocation) async {
+    final address = await LocationHelper.getPlaceAddress(
+        pickedLocation.latitude, pickedLocation.longitude);
+    final updatedLocation = PlaceLocation(
+        latitude: pickedLocation.latitude,
+        longitude: pickedLocation.longitude,
+        address: address);
+
     final newPlace = Place(
         id: DateTime.now().toString(),
         image: pickedImage,
         title: pickedTitle,
-        location: PlaceLocation(latitude: 0, longitude: 0));
+        location: updatedLocation);
 
     _items.add(newPlace);
     notifyListeners();
@@ -24,7 +33,10 @@ class GreatPlaces with ChangeNotifier {
     DBHelper.insert('user_places', {
       'id': newPlace.id,
       'title': newPlace.title,
-      'image': newPlace.image.path
+      'image': newPlace.image.path,
+      'loc_lat': newPlace.location.latitude,
+      'loc_lng': newPlace.location.longitude,
+      'address': newPlace.location.address,
     });
   }
 
@@ -36,7 +48,10 @@ class GreatPlaces with ChangeNotifier {
               id: item['id'],
               title: item['title'],
               image: File(item['image']),
-              location: PlaceLocation(latitude: 0, longitude: 0),
+              location: PlaceLocation(
+                  latitude: item['loc_lat'],
+                  longitude: item['loc_lng'],
+                  address: item['address']),
             ))
         .toList();
     notifyListeners();

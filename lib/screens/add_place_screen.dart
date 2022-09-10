80c6a68529475ef2d303de7b68144c2596e362c2
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-10 12:22:51 +0500

    Integrated LocationInput and added addplace method

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../models/place.dart';
 import '../providers/great_places.dart';
 import '../widgets/image_input.dart';
 import '../widgets/location_input.dart';
@@ -19,17 +20,24 @@ class AddPlaceScreen extends StatefulWidget {
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   final _titleController = TextEditingController();
   File? _pickedImage;
+  PlaceLocation? pickedLocation;
 
   void _selectImage(File pickedImage) {
     _pickedImage = pickedImage;
   }
 
+  void _addPlace(double lat, double long) {
+    pickedLocation = PlaceLocation(latitude: lat, longitude: long);
+  }
+
   void _savePlace() {
-    if (_titleController.text.isEmpty || _pickedImage == null) {
+    if (_titleController.text.isEmpty ||
+        _pickedImage == null ||
+        pickedLocation == null) {
       return;
     }
     Provider.of<GreatPlaces>(context, listen: false)
-        .addPlace(_titleController.text, _pickedImage!);
+        .addPlace(_titleController.text, _pickedImage!, pickedLocation!);
     Navigator.of(context).pop();
   }
 
@@ -60,7 +68,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                   const SizedBox(
                     height: 10,
                   ),
-                  const LocationInput()
+                  LocationInput(onSelectPlace: _addPlace),
                 ],
               ),
             ),

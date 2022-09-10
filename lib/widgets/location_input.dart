91984561b0b2db052a426de178dfa498b89c0be9
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-10 12:26:20 +0500

    Added support for location picked directly from screen

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../helpers/location_helper.dart';
+import '../screens/map_screen.dart';
 
 class LocationInput extends StatefulWidget {
-  const LocationInput({Key? key}) : super(key: key);
+  final Function onSelectPlace;
+
+  const LocationInput({Key? key, required this.onSelectPlace})
+      : super(key: key);
 
   @override
   State<LocationInput> createState() => _LocationInputState();
@@ -9,6 +18,42 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   String? _previewLocationUrl;
+
+  void _showPreview(double lat, double long) {
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+        latitude: lat, longitude: long);
+
+    setState(() {
+      _previewLocationUrl = staticMapImageUrl;
+    });
+  }
+
+  Future<void> _getCurrentUserLocation() async {
+    try {
+      final locData = await Location().getLocation();
+
+      _showPreview(locData.latitude!, locData.longitude!);
+
+      widget.onSelectPlace(locData.latitude, locData.longitude);
+    } catch (error) {
+      return;
+    }
+  }
+
+  Future<void> _selectOnMap() async {
+    final selectedLocation = await Navigator.of(context).push<LatLng>(
+      MaterialPageRoute(
+          fullscreenDialog: true,
+          builder: (ctx) => const MapScreen(
+                isSelecting: true,
+              )),
+    );
+
+    if (selectedLocation == null) return;
+
+    _showPreview(selectedLocation.latitude, selectedLocation.longitude);
+    widget.onSelectPlace(selectedLocation.latitude, selectedLocation.longitude);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +78,7 @@ class _LocationInputState extends State<LocationInput> {
         ),
         Row(mainAxisAlignment: MainAxisAlignment.center, children: [
           TextButton.icon(
-            onPressed: () {},
+            onPressed: _getCurrentUserLocation,
             icon: const Icon(Icons.location_on),
             label: Text(
               'Current Location',
@@ -41,7 +86,7 @@ class _LocationInputState extends State<LocationInput> {
             ),
           ),
           TextButton.icon(
-            onPressed: () {},
+            onPressed: _selectOnMap,
             icon: const Icon(Icons.map),
             label: Text(
               'Select on Map',
